92b1777deaf20850d84ded6c6d9bb5dd6efae539
dymoc <dymoc@mail.ru>
2021-06-25 09:43:24 +0300

    Подготовленны шаблоны под twig

---
+++ lesson5/task/engine/TwigRender.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app\engine;
+
+
+use app\interfaces\IRenderer;
+
+class TwigRender implements IRenderer
+{
+    public 
+
+    public function renderTemplate($template, $params = []) {
+        require_once '../vendor/autoload.php';
+
+        $loader = new \Twig\Loader\FilesystemLoader('/');
+        $twig = new \Twig\Environment($loader);
+
+        return $twig->render('template.twig', ['name' => 'Fabien']);
+    }
+}
